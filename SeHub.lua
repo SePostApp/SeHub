52041ab727fb005cff76c380d6d05cc1abfc0fd0
SePostApp <riorizky777@gmail.com>
2025-11-29 04:12:00 +0700

    Update SeHub.lua

--- SeHub.lua
+++ SeHub.lua
@@ -207,7 +207,7 @@ local function startFishing()
 
             -- 3. FishingCompleted
             net:WaitForChild("RE/FishingCompleted"):FireServer()
-            task.wait(0.1)
+            task.wait(0.05)
         end
     end)
 end
