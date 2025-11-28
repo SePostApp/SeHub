9940d6138e3d106d8b0105f304d6b85ee80251ef
SePostApp <riorizky777@gmail.com>
2025-11-29 04:07:37 +0700

    Increase wait duration in FishingCompleted event

--- SeHub.lua
+++ SeHub.lua
@@ -207,7 +207,7 @@ local function startFishing()
 
             -- 3. FishingCompleted
             net:WaitForChild("RE/FishingCompleted"):FireServer()
-            task.wait(0.0)
+            task.wait(0.1)
         end
     end)
 end
